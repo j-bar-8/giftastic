bac882528d06d82f08d2bdbcb002f087f926b6c6
j-bar-8 <josh.barkat@gmail.com>
2018-06-23 21:51:50 -0400

    logic completed

---
+++ assets/javascript/logic.js
@@ -0,0 +1,91 @@
+// GLOBAL VARIABLES
+// ===========================================================
+var things = ["cat", "dog", "r2d2", "wrx"]
+
+
+
+
+
+
+// FUNCTIONS
+// ===========================================================
+function renderButtons() {
+    $("#button-view").empty();
+
+    for (var i=0; i<things.length; i++){
+        var a = $("<button>");
+        a.addClass("something-button");
+        a.attr("data-name",things[i]);
+        a.text(things[i]);
+        $("#button-view").append(a);
+    }
+}
+
+function displayGIFS () {
+    $("#something-view").empty();
+    var thing = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      thing + "&api_key=ZP8beXwuh0QLgysjT4cvXLGm02mlbgFn&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+
+          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            var gifDiv = $("<div class='item'>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var thingImage = $("<img>");
+            thingImage.attr("src", results[i].images.fixed_height.url);
+            thingImage.attr("data-still", results[i].images.fixed_height_still.url);
+            thingImage.attr("data-animate", results[i].images.fixed_height.url);
+            thingImage.attr("data-state", "animate");
+            thingImage.attr("class", "gif");
+            
+            // TESTING
+            // console.log(results[i]);
+           
+            gifDiv.append(p);
+            gifDiv.append(thingImage);
+
+            $("#something-view").prepend(gifDiv);
+          }
+        }
+      });
+}
+
+
+
+
+// PROCESS
+// ===========================================================
+renderButtons();
+
+$("#add-something").on("click", function(event) {
+    event.preventDefault();
+    var newThing = $("#something-input").val().trim();
+    things.push(newThing);
+    renderButtons();
+    $("#something-input").val("");
+});
+
+$(document).on("click", "button", displayGIFS);
+
+$(document).on("click", ".gif", function() {
+    // TESTING
+    // console.log("clicked");
+    
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
